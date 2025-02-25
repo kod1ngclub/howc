71d31717429e3c6b57f5bdf85be201b14f74bcef
kod1ngclub <wiiwefwefbi@gmail.com>
2025-02-25 17:22:54 +0900

    BUILD: Changed from pandoc to grip(pip)

--- make.sh
+++ make.sh
@@ -1,19 +1,18 @@
-# target file
-input=docs/howc.md
-output=build/index.html
-head=build/head.html
+# ==== deps
+# - pip
+# - grip
 
+# ==== use following commands to install
+# sudo apt update
+# sudo apt install python3-pip -y
+# pip install grip
+
+# ==== target files
+INPUT=docs/howc.md
+OUTPUT=build/index.html
+
+# ==== set build directory
 mkdir build
-touch $head
 
-# write custom head
-html_keyword='<meta name="keywords" content="coding standard, kod1ngclub, C, C99, Clang" />'
-html_description='<meta name="Description" content="C coding standard of Ko1ngclub" />'
-html_style='<style> body { font-family: "Courier New", Courier, monospace; } </style>'
-
-echo $html_keyword      > $head
-echo $html_description  > $head
-echo $html_style        > $head
-
-# convert md to html
-pandoc $input -o $output --metadata title="Howc" --standalone --include-in-header=$head
+# ==== compile to HTML
+grip $INPUT --export $OUTPUT
